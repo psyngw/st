57bab129143c7b883df1b746eab7e993698c3fb1
psyngw <kyunderwarren@hotmail.com>
2022-02-16 15:40:15 +0800

    patch: font2.

--- config.h
+++ config.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+// static char *font = "SauceCodePro Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = {
+                      "JoyPixels:pixelsize=14:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
